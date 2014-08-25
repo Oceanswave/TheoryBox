f2647a0914b558c8c9ab54cfbebc1382ff21b121
Sleepyhead <sean.mclellan@sixconcepts.com>
2014-08-24 20:48:59 -0400

    missing file

---
+++ NQG.TheoryBox.Common/DomainModel/CardPT.cs
@@ -0,0 +1,21 @@
+﻿namespace NQG.TheoryBox.DomainModel
+{
+    using Newtonsoft.Json;
+
+    public class CardPT
+    {
+        [JsonProperty("p")]
+        public string P
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("t")]
+        public string T
+        {
+            get;
+            set;
+        }
+    }
+}
